34b75b53a2466dbf2223441e2599b3d885160854
Dan J <dan.jones8902@gmail.com>
2020-10-23 15:35:37 +0100

    Added teleporter for frist bit of level

---
+++ Assets/GlobalScripts/Teleporter.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Teleporter : MonoBehaviour
+{
+    public Transform PointA;
+
+
+
+	private void OnTriggerEnter(Collider other)
+	{
+		if (other.CompareTag("Player"))
+		{
+			other.transform.position = PointA.position;
+		}
+	}
+
+}
