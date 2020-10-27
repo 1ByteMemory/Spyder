e651b822564ef90e9ac0fed93033701f3476866d
Dan J <dan.jones8902@gmail.com>
2020-10-27 13:56:56 +0000

    Made system that prevents player from switching dimension if object is in the way
    Player won't be able to switch dimension if they would switch inside of an object.
    
    Next task make visual to make it obvious.

--- Assets/Player/Scripts/PlayerController.cs
+++ Assets/Player/Scripts/PlayerController.cs
@@ -2,21 +2,61 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerController : DimensionChanger
+public class PlayerController : MonoBehaviour
 {
 
     int currentDimension;
+    public Camera camA;
+    public Camera camB;
 
-    // Update is called once per frame
-    void Update()
+    public GameObject triggerDetector;
+    bool isInWall;
+
+    public int universeLayer1;
+    public int universeLayer2;
+
+
+	private void Start()
+	{
+        triggerDetector.layer = 0;
+        IgnoreLayer(universeLayer2, universeLayer1);
+    }
+
+	// Update is called once per frame
+	void Update()
     {
-        if (Input.GetMouseButtonDown(1))
+        if (Input.GetMouseButtonDown(1) && !isInWall)
         {
-            currentDimension = currentDimension == 0 ? 1 : 0;
-
-            if (GetDimension(currentDimension) == null) return;
-            transform.position = GetDimension(currentDimension).position;
-            transform.rotation = GetDimension(currentDimension).rotation;
+            if (currentDimension == 1)
+			{
+                // Switch to universe A
+                currentDimension = 0;
+                IgnoreLayer(universeLayer2, universeLayer1);
+			}
+			else
+			{
+                // Switch to universe B
+                currentDimension = 1;
+                IgnoreLayer(universeLayer1, universeLayer2);
+			}
         }
     }
+
+
+    void IgnoreLayer(int ignoreLayer, int collideLayer)
+    {
+        // 10 is the Player's layer
+        Physics.IgnoreLayerCollision(10, ignoreLayer, true); // true - do ignore these collisions
+        Physics.IgnoreLayerCollision(10, collideLayer, false); // false - do not ignore these collisions....confusing I know
+    }
+
+	private void OnTriggerEnter(Collider other)
+	{
+        isInWall = true;
+	}
+
+	private void OnTriggerExit(Collider other)
+	{
+        isInWall = false;
+	}
 }
