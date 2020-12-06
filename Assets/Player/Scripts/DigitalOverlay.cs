98e689fe4162a7ca9a94d1edf8365dd7cefd8784
Dan J <dan.jones8902@gmail.com>
2020-12-06 16:36:42 +0000

    Depth Texture

--- Assets/Player/Scripts/DigitalOverlay.cs
+++ Assets/Player/Scripts/DigitalOverlay.cs
@@ -8,10 +8,19 @@ public class DigitalOverlay : MonoBehaviour
 
     public Material digitalOverleyEffect;
 	public new bool enabled;
+	public float depthScale;
+
 
 	private void OnRenderImage(RenderTexture source, RenderTexture destination)
 	{
 		if (digitalOverleyEffect != null && enabled == true)
+		{
+			GetComponent<Camera>().depthTextureMode = DepthTextureMode.DepthNormals;
+
+			digitalOverleyEffect.SetFloat("_Scale", depthScale);
 			Graphics.Blit(source, destination, digitalOverleyEffect);
+		}
+
+		RenderTexture.active = destination;
 	}
 }
--- Assets/Shaders/DigtalImageEffect.shader
+++ Assets/Shaders/DigtalImageEffect.shader
@@ -38,13 +38,15 @@
             }
 
             sampler2D _MainTex;
+            sampler _CameraDepthTexture;
+            float _Scale;
 
             fixed4 frag (v2f i) : SV_Target
             {
-                fixed4 col = tex2D(_MainTex, i.uv);
-                // just invert the colors
-                col.rgb = 1 - col.rgb;
-                return col;
+                fixed4 col = tex2D(_CameraDepthTexture, i.uv);
+                col.gb = col.r;
+                return col * _Scale;
+
             }
             ENDCG
         }
