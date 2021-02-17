f9b26c83cb4144459d23e441327573484a1933e3
Charlotte <dan.jones8902@gmail.com>
2021-02-17 12:39:32 +0000

    Added a colour picker

---
+++ Assets/UI/Scripts/ColourPicker/CPButton.cs
@@ -0,0 +1,91 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+[ExecuteInEditMode]
+public class CPButton : MonoBehaviour
+{
+
+    public Image img;
+	public GameObject colorPanel;
+
+	public CPSlider R, G, B;
+
+	public Color color;
+
+	private Color oldColor;
+
+	private RectTransform rect;
+
+	private void Start()
+	{
+		rect = GetComponent<RectTransform>();
+	}
+
+	private void OnEnable()
+	{
+		SetColour(color);
+	}
+
+	private void OnValidate()
+	{
+		SetColour(color);
+	}
+
+
+	public void ToggleColorPicker()
+	{
+		// Check if button is clicked and not the colour display panel
+		if (RectTransformUtility.RectangleContainsScreenPoint(rect, Input.mousePosition))
+		{
+			colorPanel.SetActive(!colorPanel.activeSelf);
+
+			if (colorPanel.activeSelf)
+			{
+				oldColor = img.color;
+			}
+		}
+	}
+
+	public void OpenColorPicker()
+	{
+		oldColor = img.color;
+		colorPanel.SetActive(true);
+	}
+
+	public void CloseColorPicker()
+	{
+		colorPanel.SetActive(false);
+	}
+
+	public void CancelChanges()
+	{
+		img.color = oldColor;
+
+		R.SetValue(oldColor.r);
+		G.SetValue(oldColor.g);
+		B.SetValue(oldColor.b);
+	}
+
+	public void SetColour(Color col)
+	{
+		col.a = 1;
+		img.color = col;
+
+		R.SetValue(col.r);
+		G.SetValue(col.g);
+		B.SetValue(col.b);
+	}
+
+
+	public void ChangeColour()
+	{
+		float r = R != null ? R.value : 1;
+		float g = G != null ? G.value : 1;
+		float b = B != null ? B.value : 1;
+
+		img.color = new Color(r, g, b, 1);
+	}
+}
---
+++ Assets/UI/Scripts/ColourPicker/CPSlider.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
+
+public enum Colour
+{
+	Red,
+	Green,
+	Blue
+}
+
+[RequireComponent(typeof(Slider))]
+[ExecuteInEditMode]
+public class CPSlider : MonoBehaviour
+{
+	public Colour colour;
+
+	[HideInInspector]
+	public float value;
+
+	public TextMeshProUGUI text;
+
+	private Slider slider;
+
+	public void OnEnable()
+	{
+		slider = GetComponent<Slider>();
+
+		if (text != null)
+		{
+			text.text = Mathf.FloorToInt(slider.value * 255).ToString();
+		}
+		value = slider.value;
+	}
+
+	public void SetValue(float value)
+	{
+		this.value = value;
+
+		if (text != null)
+		{
+			// Convert value to 0-255
+			int bit = Mathf.FloorToInt(value * 255);
+
+			text.text = bit.ToString();
+		}
+
+
+		// If the value is being changed else where
+		if (slider != null && value != slider.value)
+		{
+			slider.value = value;
+		}
+	}
+
+}
