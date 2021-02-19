40449986962a6efff89f5a63ee4e50ed151f7b65
Charlotte <dan.jones8902@gmail.com>
2021-02-19 13:43:34 +0000

    Trying to get the settings to save and load from a file

---
+++ Assets/GlobalScripts/JSONScripts/JsonIO.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class JsonIO : MonoBehaviour
+{
+    public PlayerSettings playerSettings = new PlayerSettings();
+
+	public Text text;
+
+	private void Start()
+	{
+		LoadSettings();
+	}
+
+	public void Update()
+	{
+		if (Input.GetKeyDown(KeyCode.Space))
+		{
+			playerSettings.feildOfView = 90;
+			playerSettings.outlines = Color.green;
+
+			
+			SaveSettings(playerSettings);
+		}
+
+		if (Input.GetKeyDown(KeyCode.B))
+		{
+			LoadSettings();
+
+		}
+		text.text = playerSettings.feildOfView.ToString();
+	}
+
+	public void LoadSettings()
+	{
+		Debug.Log("Loading Settings");
+		var jsonTextFile = Resources.Load<TextAsset>("settings");
+		playerSettings = PlayerSettings.CreateFromJson(jsonTextFile.ToString());
+		Debug.Log("Settings Loaded");
+		Debug.Log(playerSettings.ToString());
+	}
+
+	public void SaveSettings(PlayerSettings settings)
+	{
+		string jsonTextFile = settings.SaveToString();
+
+		Debug.Log("Saving Settings");
+		StreamWriter writer = new StreamWriter(Application.dataPath + "/Resources/settings.json");
+
+		Debug.Log("Writing line");
+		writer.WriteLine(jsonTextFile);
+		writer.Close();
+		
+
+	}
+}
---
+++ Assets/GlobalScripts/JSONScripts/PlayerSettings.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using UnityEngine;
+
+[Serializable]
+public class PlayerSettings
+{
+	// General
+	public int feildOfView;
+	public int difficulty;
+
+	//Controls
+	public float lookSensitivity;
+	public float scrollSensitivity;
+
+	// Colors
+	public Color outlines;
+	public Color background;
+	public Color enemyOutline;
+
+	// Video
+	public bool isFullscreen;
+	public int resolution;
+
+	// Audio
+	public float enemyVoice;
+	public float soundFX;
+	public float music;
+	public float dialogue;
+	public float ambience;
+
+	// Accessability
+	public bool epilepticMode;
+	public bool toggelCrouch;
+	public bool timeSlow;
+
+
+	public string SaveToString()
+	{
+		return JsonUtility.ToJson(this);
+	}
+
+	public static PlayerSettings CreateFromJson(string jsonString)
+	{
+		return JsonUtility.FromJson<PlayerSettings>(jsonString);
+	}
+
+}
