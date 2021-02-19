0ed0f7636bb839cedb8036f963bfcc9705af9ba2
Charlotte <dan.jones8902@gmail.com>
2021-02-19 10:50:41 +0000

    Pixel effect works perfectly

--- Assets/GlobalScripts/Pixelate.cs
+++ Assets/GlobalScripts/Pixelate.cs
@@ -6,9 +6,7 @@ using UnityEngine.Rendering.PostProcessing;
 [PostProcess(typeof(PixelateRenderer), PostProcessEvent.AfterStack, "Custom/Pixelate")]
 public sealed class Pixelate : PostProcessEffectSettings
 {
-	//[Range(0f, 10f)]
-	public FloatParameter blend = new FloatParameter { value = 0.5f };
-	public Vector3Parameter color = new Vector3Parameter { value = new Vector3(1, 1, 1) };
+	public Vector2Parameter ratio = new Vector2Parameter { value = new Vector2(320, 240) };
 }
 
 public sealed class PixelateRenderer : PostProcessEffectRenderer<Pixelate>
@@ -16,8 +14,7 @@ public sealed class PixelateRenderer : PostProcessEffectRenderer<Pixelate>
 	public override void Render(PostProcessRenderContext context)
 	{
 		var sheet = context.propertySheets.Get(Shader.Find("Hidden/Custom/Pixelate"));
-		sheet.properties.SetFloat("_Blend", settings.blend);
-		sheet.properties.SetVector("_Color", settings.color);
+		sheet.properties.SetVector("_Ratio", settings.ratio);
 		context.command.BlitFullscreenTriangle(context.source, context.destination, sheet, 0);
 	}
 }
