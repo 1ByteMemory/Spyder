1339a979a98849fd66709432b4867fd815404798
Daniel Truman <dt217690@falmouth.ac.uk>
2021-03-06 16:50:37 +0000

    Funicular Movement Update
    you don't slide on the funicular anymore

--- Assets/Environment/Animations/FunicularController.cs
+++ Assets/Environment/Animations/FunicularController.cs
@@ -8,6 +8,7 @@ public class FunicularController : MonoBehaviour
 	Animator anim;
 
 
+
 	private void Start()
 	{
 		anim = GetComponent<Animator>();
@@ -18,20 +19,28 @@ public class FunicularController : MonoBehaviour
 		anim.SetBool("Play", true);
 	}
 
+	void OnTriggerEnter(Collider collision)
+	{
+		collision.GetComponent<Collider>().transform.SetParent(transform);
+	}
+
+	void OnTriggerExit(Collider collision)
+	{
+		collision.GetComponent<Collider>().transform.SetParent(null);
+	}
 
 	// These methods are called from the animator event keys
 	public void Go()
 	{
-		GameObject.FindGameObjectWithTag("Player").transform.parent = transform;
+		//	GameObject.FindGameObjectWithTag("Player").GetComponent<Collider>().transform.SetParent(transform);
 	}
 	public void Stop()
 	{
-		GameObject.FindGameObjectWithTag("Player").transform.parent = null;
+		//	GameObject.FindGameObjectWithTag("Player").GetComponent<Collider>().transform.SetParent(null);
 	}
 	public void StopAnim()
 	{
 		anim.speed = 0;
 	}
 
-	
 }
