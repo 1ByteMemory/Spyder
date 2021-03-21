f11d4fad22d44ad5d4a94cba7b86ea6a2c689190
Daniel Truman <dt217690@falmouth.ac.uk>
2021-03-21 04:53:29 +0000

    Update
    attempted to add a moveonobject script to make puzzle more interesting
    *POTENTIAL BUG* player no longer goes into the digital world when picks up pistol at the start of the game. Don't know why as haven't touched anything. (possibly something to do with previous script)
    removed player shadows are they aren't needed and just feel bad
    added gap to the bottom of funicular to make it harder to skip

---
+++ Assets/moveonobjects.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class moveonobjects : MonoBehaviour
+{
+
+   private void OnTriggerEnter(Collider collision)
+    {
+        collision.GetComponent<Collider>().transform.SetParent(transform);
+    }
+
+    private void OnTriggerExit(Collider collision)
+    {
+        collision.GetComponent<Collider>().transform.SetParent(null);
+    }
+
+
+}
