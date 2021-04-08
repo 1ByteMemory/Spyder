c7c6fd10075c0c69c9dfe20d37d40877b888007d
Charlotte <dj220679@falmouth.ac.uk>
2021-04-08 19:47:23 +0100

    Started reworking ui elements to work with quicksaves

---
+++ Assets/GlobalScripts/Checkpoints.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Checkpoints : MonoBehaviour
+{
+	LevelInfo[] quickSaves;
+	LevelInfo[] checkpoints;
+	LevelSelection selection;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+     
+		if (Input.GetKey(KeyCode.F5))
+		{
+			// Quick Save
+
+			
+
+		}
+
+		if (Input.GetKey(KeyCode.F6))
+		{
+			// Quick Load
+
+
+		}
+    }
+}
