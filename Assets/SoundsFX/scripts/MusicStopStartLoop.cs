d64df2207cf6a47fec0319c2d8f93876bdadd49c
Daniel Truman <dt217690@falmouth.ac.uk>
2021-05-11 17:35:42 +0100

    SoundChanges and additions
    added sound that loops and one that turns off the loop also
    these are used ot build tension, also added sounds for those

---
+++ Assets/SoundsFX/scripts/MusicStopStartLoop.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicStopStartLoop : MonoBehaviour
+{
+
+    public AudioClip combat2;
+    private AudioSource combatsource2;
+    //public AudioSource combatsource;
+
+
+    void Start()
+    {
+        combatsource2 = GameObject.Find("Music").GetComponent<AudioSource>();
+        //GetComponent<AudioSource> ().playOnAwake.false;
+        //GetComponent<AudioSource> ().clip = combat
+    }
+
+
+
+    // Start is called before the first frame update
+    void OnTriggerEnter()
+    {
+        combatsource2.clip = combat2;
+        combatsource2.Play();
+        combatsource2.loop = true;
+        gameObject.SetActive(false);
+        //combatsource.clip = combat;
+        //combatsource.Play();
+    }
+
+    // Update is called once per frame
+    public void OnTriggerExit()
+    {
+        //combatsource.clip = combat;
+        //combatsource.Play();
+        //gameObject.SetActive(false);
+        //GetComponent<AudioSource>().Play();
+    }
+}
