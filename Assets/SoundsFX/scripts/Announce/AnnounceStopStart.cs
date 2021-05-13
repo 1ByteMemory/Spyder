2b2ebc61fb29b3eef775eca110783cffae19206e
Daniel Truman <dt217690@falmouth.ac.uk>
2021-05-13 16:52:23 +0100

    Announcer Implementation
    Announcer on seperate audio source to back ground music. uses same system with collider boxes

---
+++ Assets/SoundsFX/scripts/Announce/AnnounceStopStart.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnnounceStopStart : MonoBehaviour
+{
+
+    public AudioClip Announcement;
+    private AudioSource Announcesource;
+    //public AudioSource combatsource;
+
+
+    void Start()
+    {
+        Announcesource = GameObject.Find("Announce").GetComponent<AudioSource>();
+        //GetComponent<AudioSource> ().playOnAwake.false;
+        //GetComponent<AudioSource> ().clip = combat
+    }
+
+
+
+    // Start is called before the first frame update
+    void OnTriggerEnter()
+    {
+        Announcesource.clip = Announcement;
+        Announcesource.Play();
+        gameObject.SetActive(false);
+        //combatsource.clip = combat;
+        //combatsource.Play();
+    }
+
+    // Update is called once per frame
+    public void OnTriggerExit()
+    {
+        //combatsource.clip = combat;
+        //combatsource.Play();
+        //gameObject.SetActive(false);
+        //GetComponent<AudioSource>().Play();
+    }
+}
